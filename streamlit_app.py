34ae16f5bc8bcfdf9ecb2e43e44c9344cc806db0
NicolasAversa <nicolasaversadm@gmail.com>
2025-07-24 19:18:20 -0300

    Add list to purchase

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,30 @@
 import streamlit as st
-from lib.yu_gi_oh_editor import create_cards_dataset, read_card_dataset, all_decks
 import pandas as pd
+from lib.yu_gi_oh_editor import get_all_decks, get_all_cards_dataframe
 
-cards = read_card_dataset()
-cards_df = create_cards_dataset(cards)
+cards_df = get_all_cards_dataframe()
+all_decks = get_all_decks()
 
-decks_options = sorted(all_decks.keys())
 
-selected_deck = st.selectbox(label="Select your deck", options=decks_options)
-selected_deck_df = all_decks[selected_deck]
-merged_deck_df = pd.merge(how="left", left=selected_deck_df, right=cards_df, on="id")
+character_options = sorted([key.capitalize() for key in all_decks.keys()])
+selected_character = st.selectbox("Select a character", character_options)
 
-merged_deck_sorted_df = merged_deck_df.sort_values(
-    by=["type", "race", "atk", "def", "id"], ascending=[True, True, False, False, True]
-).reset_index(drop=True)
+character_deck_dfs = all_decks[selected_character.lower()]
+sorted_deck_numbers = sorted(character_deck_dfs.keys(), key=lambda x: int(x))
 
-st.dataframe(merged_deck_sorted_df)
+deduplicated_decks = []
+seen_ids = set()
+
+for deck_number in sorted_deck_numbers:
+    deck_df = character_deck_dfs[deck_number]
+    filtered_df = deck_df[~deck_df["id"].isin(seen_ids)].copy()
+    deduplicated_decks.append((deck_number, filtered_df))
+    seen_ids.update(filtered_df["id"].tolist())
+
+for deck_number, deck_df in deduplicated_decks:
+    st.write(f"Deck {deck_number}")
+    merged_deck_df = pd.merge(how="left", left=deck_df, right=cards_df, on="id")
+    merged_deck_sorted_df = merged_deck_df.sort_values(
+        by=["type", "race", "atk", "def", "id"], ascending=[True, True, False, False, True]
+    ).reset_index(drop=True)
+    st.dataframe(merged_deck_sorted_df)
